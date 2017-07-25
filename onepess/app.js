97d07dee4ed7f000091706f60463b37c56c39857
Hojin Seo <micom96@hanmail.net>
2017-07-25 17:27:22 +0900

    update

--- onepess/app.js
+++ onepess/app.js
@@ -74,8 +74,13 @@ app.post("/webhook", function(req, res) {
 function receivedMessage(event) {
 	var senderId = event.sender.id;
 	var content = event.message.text;
-	var echo_message = "ECHO : " + content;
-	sendTextMessage(senderId, echo_message);
+
+	if (content === 'choose') {
+		sendGenericMessage(senderId);
+	} else {
+		var echo_message = "ECHO : " + content;
+		sendTextMessage(senderId, echo_message);
+	}
 }
 function receivedPostback(event) {
 	console.log("RECEIVED POSTBACK IT WORKS");
@@ -106,44 +111,44 @@ function sendTextMessage(recipientId, message) {
 function sendGenericMessage(recipientId) {
 	var messageData = {
 		recipient: {
-		  id: recipientId
+		id: recipientId
 		},
 		message: {
-		  attachment: {
+		attachment: {
 			type: "template",
 			payload: {
-			  template_type: "generic",
-			  elements: [{
+			template_type: "generic",
+			elements: [{
 				title: "rift",
 				subtitle: "Next-generation virtual reality",
 				item_url: "https://www.oculus.com/en-us/rift/",
 				image_url: "http://messengerdemo.parseapp.com/img/rift.png",
 				buttons: [{
-				  type: "web_url",
-				  url: "https://www.oculus.com/en-us/rift/",
-				  title: "Open Web URL"
+				type: "web_url",
+				url: "https://www.oculus.com/en-us/rift/",
+				title: "Open Web URL"
 				}, {
-				  type: "postback",
-				  title: "Call Postback",
-				  payload: "Payload for first bubble",
+				type: "postback",
+				title: "Call Postback",
+				payload: "Payload for first bubble",
 				}],
-			  }, {
+			}, {
 				title: "touch",
 				subtitle: "Your Hands, Now in VR",
 				item_url: "https://www.oculus.com/en-us/touch/",
 				image_url: "http://messengerdemo.parseapp.com/img/touch.png",
 				buttons: [{
-				  type: "web_url",
-				  url: "https://www.oculus.com/en-us/touch/",
-				  title: "Open Web URL"
+				type: "web_url",
+				url: "https://www.oculus.com/en-us/touch/",
+				title: "Open Web URL"
 				}, {
-				  type: "postback",
-				  title: "Call Postback",
-				  payload: "Payload for second bubble",
+				type: "postback",
+				title: "Call Postback",
+				payload: "Payload for second bubble",
 				}]
-			  }]
+			}]
 			}
-		  }
+		}
 		}
 	};
 
