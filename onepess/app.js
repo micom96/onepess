b9338b12c82d72899cde1692ae34233c87e8e32d
Hojin Seo <micom96@hanmail.net>
2017-07-25 17:30:04 +0900

    update

--- onepess/app.js
+++ onepess/app.js
@@ -154,6 +154,31 @@ function sendGenericMessage(recipientId) {
 
 	callSendAPI(messageData);
 	}
+
+function callSendAPI(messageData) {
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: { access_token: PAGE_ACCESS_TOKEN },
+		method: 'POST',
+		json: messageData
+
+	}, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+		var recipientId = body.recipient_id;
+		var messageId = body.message_id;
+
+		if (messageId) {
+			console.log("Successfully sent message with id %s to recipient %s",
+			messageId, recipientId);
+		} else {
+		console.log("Successfully called Send API for recipient %s",
+			recipientId);
+		}
+		} else {
+		console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+		}
+	});
+	}
 /*
 function receivedAuthentication(event) {
 	console.log("RECEIVED AUTHENTICATION");
